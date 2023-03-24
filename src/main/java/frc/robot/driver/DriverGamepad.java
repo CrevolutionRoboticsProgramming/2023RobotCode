e1b8901363dfd4594749cdb9c81990f783995d2b
jmdeal <jasond@dde-inc.com>
2023-03-24 02:55:32 -0400

    feat: drivetrain working??

--- src/main/java/frc/robot/driver/DriverGamepad.java
+++ src/main/java/frc/robot/driver/DriverGamepad.java
@@ -1,15 +1,22 @@
 package frc.robot.driver;
 
+import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.SpectrumLib.gamepads.Gamepad;
 import frc.robot.RobotContainer;
+import frc.robot.drivetrain.commands.DrivetrainCommands;
 import frc.robot.intake.commands.*;
 
 public class DriverGamepad extends Gamepad {
     public DriverGamepad() {
         super("DriverController", DriverConfig.kDriverPort);
+
+        gamepad.leftStick.setDeadband(0.1);
+        gamepad.rightStick.setDeadband(0.1);
+
+        gamepad.rightStick.configCurves(2, 1);
     }
 
     @Override
@@ -17,9 +24,17 @@ public class DriverGamepad extends Gamepad {
         // Drivetrain Input
         gamepad.startButton.onTrue(new InstantCommand(() -> RobotContainer.drivetrain.zeroGyro()));
 
+        gamepad.aButton.onTrue(new InstantCommand(() -> RobotContainer.intakePivot.setIdleMode(CANSparkMax.IdleMode.kCoast)));
+        gamepad.bButton.onTrue(new InstantCommand(() -> RobotContainer.intakePivot.setIdleMode(CANSparkMax.IdleMode.kBrake)));
+
         // Intake input
-        gamepad.rightTriggerButton.whileTrue(new RunIntake(RobotContainer.intakeRoller, RunIntake.Mode.kCone));
-        gamepad.leftTriggerButton.whileTrue(new RunIntake(RobotContainer.intakeRoller, RunIntake.Mode.kCube));
+//        gamepad.rightTriggerButton.whileTrue(new RunIntake(RobotContainer.intakeRoller, RunIntake.Mode.kCone));
+//        gamepad.leftTriggerButton.whileTrue(new RunIntake(RobotContainer.intakeRoller, RunIntake.Mode.kCube));
+        gamepad.leftTriggerButton.whileTrue(DrivetrainCommands.driveFieldOrientedSlow(
+                this::getDriveTranslationX,
+                this::getDriveTranslationY,
+                this::getDriveRotation
+        ));
     }
 
     @Override
@@ -56,5 +71,17 @@ public class DriverGamepad extends Gamepad {
 
     public double getLeftTriggerRaw() {
         return gamepad.getRawAxis(XboxController.Axis.kLeftTrigger.value);
+    }
+
+    public double getDriveTranslationX() {
+        return -gamepad.leftStick.getX();
+    }
+
+    public double getDriveTranslationY() {
+        return gamepad.leftStick.getY();
+    }
+
+    public double getDriveRotation() {
+        return gamepad.rightStick.getX();
     }
 }
